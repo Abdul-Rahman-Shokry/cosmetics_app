edb469c22a70779e768c4af31c1af8186b1bfd93
Abdul-Rahman-Shokry <abdul.rahman.shokry2004@gmail.com>
2026-06-28 11:13:29 +0300

    refactor(utils): update app_image

--- lib/core/widgets/app_image.dart
+++ lib/core/widgets/app_image.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -20,55 +21,76 @@ class AppImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (path.contains("com.example.cosmetics_app/cache")) {
+    if (path.contains("com.example.avon/cache")) {
       return Image.file(
         File(path),
         height: height,
         width: width,
         fit: fit,
-        errorBuilder: (context, error, stackTrace) => _errorWidget(),
         color: color,
+        errorBuilder: (context, error, stackTrace) => _errorWidget(),
       );
     } else if (path.endsWith("svg")) {
-      return SvgPicture.asset(
-        "assets/svg/$path",
-        height: height,
-        width: width,
-        fit: fit,
-        colorFilter: color != null
-            ? ColorFilter.mode(
-          color!,
-          BlendMode.srcIn,
-        )
-            : null,
+      return FutureBuilder(
+        future: DefaultAssetBundle.of(context).load("assets/svg/$path"),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return SizedBox(
+              height: height ?? 24,
+              width: width ?? 24,
+            );
+          }
+          if (snapshot.hasError) {
+            return _errorWidget();
+          }
+          return SvgPicture.asset(
+            "assets/svg/$path",
+            height: height,
+            width: width,
+            fit: fit,
+            colorFilter: color != null
+                ? ColorFilter.mode(
+              color!,
+              BlendMode.srcIn,
+            )
+                : null,
+          );
+        },
       );
     } else if (path.startsWith("http")) {
-      return Image.network(
-        path,
+      return CachedNetworkImage(
+        imageUrl: path,
         height: height,
         width: width,
-        errorBuilder: (context, error, stackTrace) => _errorWidget(),
         fit: fit,
         color: color,
+        placeholder: (context, url) => SizedBox(
+          height: height ?? 24,
+          width: width ?? 24,
+          child: const Center(
+            child: CircularProgressIndicator.adaptive(),
+          ),
+        ),
+        errorWidget: (context, url, error) => _errorWidget(),
       );
     } else if (path.endsWith("png") || path.endsWith("jpg")) {
       return Image.asset(
         "assets/images/$path",
         height: height,
         width: width,
-        errorBuilder: (context, error, stackTrace) => _errorWidget(),
         fit: fit,
         color: color,
+        errorBuilder: (context, error, stackTrace) => _errorWidget(),
       );
     }
     return _errorWidget();
   }
 
   Widget _errorWidget() {
-    return AppImage(
-      "image_failed.png",
-      height: height,
-      width: width,
+    return Icon(
+      Icons.error_outline_rounded,
+      color: Colors.red,
+      size: width ?? height ?? 24,
     );
   }
 }
